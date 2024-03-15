b5d697a880ea2217f8ac913e96a90cc9b3fcff85
Aktar9775 <154451758+Aktar9775@users.noreply.github.com>
2024-03-15 17:25:26 +0000

    container config done

--- 01_Basic_JS/02_datatype.js
+++ 01_Basic_JS/02_datatype.js
@@ -1,5 +1,27 @@
 "use strict";// treat as all js code newer version
-alert(3+3) // we are using nodejs not browser
+//alert(3+3) // we are using nodejs not browser
 console.log(3
     +
     3); //code readablity that should be high
+
+    console.log("Sohel");
+    let name="Sohel Aktar";
+    let age=24
+    let islogin=false
+    /*
+    number 
+    bigint
+    string=>""
+    boolean=>true/false
+    null=> standalone value
+    undefine=>
+    symbol=>unique
+
+
+    object
+    */
+   console.log(typeof undefined);//undefined
+   console.log(typeof null);//object
+
+
+

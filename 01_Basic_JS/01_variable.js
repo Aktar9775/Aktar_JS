97920b5c0a20787bff2218a19cb8ecadef8e2a33
Aktar9775 <154451758+Aktar9775@users.noreply.github.com>
2024-03-15 16:58:31 +0000

    container config done

---
+++ 01_Basic_JS/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=144552
+let accountEmail="Sohel@gmail.com"
+var accountPassword="Sijk&&8"
+let accountState; //it is undefine
+// accountID=123456 //not allowed
+//{}--> scope
+accountCity="Kolkata" // its not peferable but  you can use
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+/* 
+prefer not use var 
+because of issue for block scope and funtional scope
+*/
---
+++ 01_Basic_JS/02_datatype.js
@@ -0,0 +1,5 @@
+"use strict";// treat as all js code newer version
+alert(3+3) // we are using nodejs not browser
+console.log(3
+    +
+    3); //code readablity that should be high

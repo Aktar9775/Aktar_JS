eb2cb7e5aed1f77f8692346e959874fef5e6c4fc
Aktar9775 <154451758+Aktar9775@users.noreply.github.com>
2024-03-15 17:53:46 +0000

    conversion of datatype

---
+++ 01_Basic_JS/conversionOperation.js
@@ -0,0 +1,23 @@
+let score="Sohel"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "90"=>90
+// NaN=> Not A number
+//true=>1; false=>0
+
+let islogin="1"
+let booleanIsLoggedOn=Boolean(islogin)
+console.log(booleanIsLoggedOn)
+
+// ""=> false
+// "sohel"=>ture
+ 
+let someNumber=90
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

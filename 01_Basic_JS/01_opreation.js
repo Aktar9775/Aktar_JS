7790137c898866e358c277e20de47da0af5577dc
Aktar9775 <154451758+Aktar9775@users.noreply.github.com>
2024-03-15 18:27:01 +0000

    Operation & operator

---
+++ 01_Basic_JS/01_opreation.js
@@ -0,0 +1,15 @@
+
+//+++++++++++++++++++ Operations +++++++++++++++++
+let value=3
+let negValue=-value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);//2 pow 3
+console.log(2/3);
+console.log(2%3);
+ let str="Sohel"
+ let str1="Akter"
+ let str2= str+str1;
+ console.log(str2);

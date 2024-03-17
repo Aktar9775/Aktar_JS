4e5fbf49e4c335b9d444ea05ba0b19d0f1b4cefc
Aktar9775 <154451758+Aktar9775@users.noreply.github.com>
2024-03-17 09:59:23 +0000

    About String in JS

---
+++ 01_Basic_JS/03_string.js
@@ -0,0 +1,8 @@
+let Name="Sohel"
+let surname="Aktar"
+console.log(Name+surname+" Age 24");
+
+console.log(`Hello my name is ${Name} ${surname} and my age is 23 `);
+const Kame= new String("jkjk");
+console.log(Name.length + 4);
+

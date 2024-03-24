3889ce027efd523d8a400f2179c53564f47e7f82
Aktar9775 <154451758+Aktar9775@users.noreply.github.com>
2024-03-24 09:28:15 +0000

    DateString & function

---
+++ 01_Basic_JS/05_Date.js
@@ -0,0 +1,7 @@
+let mydate =new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+let createdate= new Date(2023,0,23);
+console.log(createdate.to);

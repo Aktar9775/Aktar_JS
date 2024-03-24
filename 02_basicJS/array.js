8e5f78b9c9e39138b7773d045f673c0bf6373207
Aktar9775 <154451758+Aktar9775@users.noreply.github.com>
2024-03-24 10:31:46 +0000

    Array_01

---
+++ 02_basicJS/array.js
@@ -0,0 +1,26 @@
+// //Array
+const arr=[0,1,2,3,4,5]
+// console.log(arr);
+// console.log(arr[1]);
+// arr.push(89);
+// console.log(arr);
+// arr.pop();// last value remove
+// console.log(arr);
+// arr.unshift(90) //add at front at array
+// console.log(arr);
+// arr.shift()// remove first value
+// arr.shift() 
+// console.log(arr);
+// console.log(arr.includes(8));// 8 include or not at arr
+// console.log(arr.indexOf(5));
+
+// const newarr=arr.join()
+// console.log(arr);
+// console.log(newarr);
+// slice ,splice
+console.log("A",arr);
+console.log(arr.slice(1,3));// Used for a portion to print
+console.log("B",arr);
+console.log(arr.splice(1,3)); // used for a some portion to out of array 
+console.log(arr); // after splice the remain values in orginal array
+
